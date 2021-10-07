18d465b9efc194c33b61e3a2193fa01ff62456de
Denilson Mucavele <77127716+Denilson87@users.noreply.github.com>
2021-10-06 23:33:30 -0700

    changing the function to async await

--- js/lyrics.js
+++ js/lyrics.js
@@ -12,46 +12,44 @@ form.addEventListener('submit', el =>{
 
 })
 //criando a funcao para fazer a submicao da nossa query usando ''then'' 
-function doSubmit() {
+//function doSubmit() {
+
+   // const lyrics_el = document.querySelector("#lyrics");
+    //const artist = document.querySelector("#artist");
+    //const song = document.querySelector("#song");
+
+    //lyrics_el.innerHTML = '<div class="spinner-border" role="status"><span class="visually-hidden">Carregando...</span></div>';
+    // fazendo uma chamada usando ''then''
+   // findLyrics(artist.value, song.value)
+      //  .then(response => response.json())
+      //  .then(data => {
+
+        //    if (data.lyrics) {
+         ///       lyrics_el.innerHTML = data.lyrics;
+            ///} else {
+               /// lyrics_el.innerHTML = data.error;
+            ///}
+        ///})
+        ///.catch(err => {
+          ///  lyrics_el.innerHTML = `opps! ${err}`;
+        ///})
+        ///}
+
+
+//criando a funcao para fazer a submicao da nossa query
+//usando async await 
+ async function doSubmit() {
 
     const lyrics_el = document.querySelector("#lyrics");
     const artist = document.querySelector("#artist");
     const song = document.querySelector("#song");
 
     lyrics_el.innerHTML = '<div class="spinner-border" role="status"><span class="visually-hidden">Carregando...</span></div>';
-    // fazendo uma chamada usando ''then''
-    findLyrics(artist.value, song.value)
-        .then(response => response.json())
-        .then(data => {
-
-            if (data.lyrics) {
-                lyrics_el.innerHTML = data.lyrics;
-            } else {
-                lyrics_el.innerHTML = data.error;
-            }
-        })
-        .catch(err => {
-            lyrics_el.innerHTML = `opps! ${err}`;
-        })
-        
-
-}
-
-
-//criando a funcao para fazer a submicao da nossa query
-
-// async function doSubmit() {
-
-//     const lyrics_el = document.querySelector("#lyrics");
-//     const artist = document.querySelector("#artist");
-//     const song = document.querySelector("#song");
-
-//     lyrics_el.innerHTML = '<div class="spinner-border" role="status"><span class="visually-hidden">Carregando...</span></div>';
   
-//         // fazendo uma chamada usando ''async await''
+        // fazendo uma chamada usando ''async await''
 
-//             const lyricsResponse  = await findLyrics(artist.value, song.value);
-//             const data = await lyricsResponse.json();
-//             lyrics_el.innerHTML = data.lyrics;
+             const lyricsResponse  = await findLyrics(artist.value, song.value);
+             const data = await lyricsResponse.json();
+             lyrics_el.innerHTML = data.lyrics;
 
-// }
+ }
